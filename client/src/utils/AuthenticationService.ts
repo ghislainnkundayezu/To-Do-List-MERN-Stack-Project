26aa05efda0f84b22710818cacca9e9df931f890
ghislainnkundayezu <ghislainnkundayezu@gmail.com>
2024-05-29 11:09:47 +0200

    configured the client to use public url

--- client/src/utils/AuthenticationService.ts
+++ client/src/utils/AuthenticationService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:  "http://192.168.1.76:3000/api/v1/",
+    baseURL:   `${process.env.BASE_URL}/api/v1/`,
     withCredentials: true,
 });
 
